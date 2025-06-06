0fcf223fd9aa292ae02837091635df9e3c560468
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2025-06-06 13:13:33 +0200

    98. 🎯 Master Signal-Based Queries in Angular 18! 🚀 No More Undefined Values in Your Components

--- tutorialWithSignalsConcept/src/app/components/computed-signal15/computed-signal15.component.ts
+++ tutorialWithSignalsConcept/src/app/components/computed-signal15/computed-signal15.component.ts
@@ -1,67 +1,24 @@
-import {
-  Component,
-  contentChild,
-  effect,
-  ElementRef,
-  Signal,
-} from '@angular/core';
-import { WidgetComponent } from '../widget/widget.component';
+import { AfterViewInit, Component, signal, viewChild } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-computed-signal15',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './computed-signal15.component.html',
   styleUrl: './computed-signal15.component.scss',
 })
-export class ComputedSignal15Component {
-  //! Using contentChild to get a reference to the "#h1"element using "h1" string below
-  //! This will allow us to access the header element in the effect
-  //! Note: contentChild is used to get a reference to the element in the content projection
-  //! This is useful when you want to access an element that is projected into the component directly
-  header1El: Signal<ElementRef<HTMLHeadingElement> | undefined> =
-    contentChild<ElementRef<HTMLHeadingElement>>('h1');
-  header2El: Signal<ElementRef<HTMLHeadingElement> | undefined> = contentChild<
-    ElementRef<HTMLHeadingElement>
-  >('h2', { descendants: true });
+export class ComputedSignal15Component implements AfterViewInit {
+  requiredEl = viewChild.required('requiredEl');
+  optionalEl = viewChild('optionalEl');
+  showEl = signal(false);
 
-  //! Using contentChild to get a reference to the WidgetComponent
-  widget: Signal<WidgetComponent | undefined> = contentChild(WidgetComponent);
-  widget2: Signal<WidgetComponent | undefined> = contentChild(WidgetComponent, {
-    read: ElementRef,
-  });
+  ngAfterViewInit(): void {
+    console.log('requiredEl: ', this.requiredEl());
+    console.log('optionalEl', this.optionalEl());
+  }
 
-  constructor() {
-    // effect need a injector context to run properly so we use the constructor
-    effect(() => {
-      const header1 = this.header1El();
-      const header2 = this.header2El();
-      const widget = this.widget();
-      const widget2 = this.widget2();
-
-      if (header1) {
-        console.log('Header1 ElementRef:', header1);
-      } else {
-        console.log('Header1 element not found');
-      }
-
-      if (header2) {
-        console.log('Header2 ElementRef:', header2);
-      } else {
-        console.log('Header2 element not found');
-      }
-
-      if (widget) {
-        console.log('Widget Component:', widget);
-      } else {
-        console.log('Widget element not found');
-      }
-
-      if (widget2) {
-        console.log('Widget2 ElementRef:', widget2);
-      } else {
-        console.log('Widget2 element not found');
-      }
-    });
+  toggleEl() {
+    this.showEl.update((value) => !value);
   }
 }
